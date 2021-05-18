7fc9e71a348743b9d3df811de012954625afb432
s0uravpandey <59795737+s0uravpandey@users.noreply.github.com>
2021-05-18 14:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import pandas as pd
 
 app = Flask(__name__)
-model = pickle.load(open('India/ind_svr_model.pkl', 'rb'))
+model = pickle.load(open('india\ind_svr_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -15,7 +15,9 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    dataset = pd.read_csv('India/ind_vaccinations.csv')
+    date = request.form['prediction_date']
+    day=((int(date[5])*10+int(date[6]))-1)*30+(int(date[8])*10+int(date[9]))-12
+    dataset = pd.read_csv('india\ind_vaccinations.csv')
     X = dataset.iloc[0:88,5].values
     y = dataset.iloc[0:112,3].values
     y=y.reshape(-1,1)
@@ -25,9 +27,9 @@ def predict():
     sc_X=StandardScaler()
     X=sc_X.fit_transform(X)
     y=sc_y.fit_transform(y)
-    y_pred=sc_y.inverse_transform(model.predict(sc_X.transform(np.array([[int(x) for x in request.form.values()]]))))
+    y_pred=sc_y.inverse_transform(model.predict(sc_X.transform(np.array([[day]]))))
     output=round(y_pred[0])
-    return render_template('vaccine_Prediction_final.html',prediction_text=output,prediction_percent=round(output/13000000))
+    return render_template('vaccine_Prediction_final.html',prediction_date=date,prediction_text=output,prediction_percent=round(output/13000000))
     
 
 if __name__ == "__main__":
